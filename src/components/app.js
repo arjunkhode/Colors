a5c2faa9f4fbbeeb0cd1761570293dd6491dd260
arjunkhode <arjunkhode@gmail.com>
2017-05-13 15:10:05 -0700

    Got the numColors switching working

--- src/components/app.js
+++ src/components/app.js
@@ -10,20 +10,47 @@ class App extends Component {
 		this.setNumColorsTo3 = this.setNumColorsTo3.bind(this);
 	}
 
+componentDidMount(){
+	const secondColor = document.querySelector('.two');
+	const thirdColor = document.querySelector('.three');
+	if(this.props.numColors === 1){
+		secondColor.style.display = 'none';
+		thirdColor.style.display = 'none';
+	}if(this.props.numColors === 2){
+		secondColor.style.display = 'block';
+		thirdColor.style.display = 'none';
+	}if(this.props.numColors === 3){
+		secondColor.style.display = 'block';
+		thirdColor.style.display = 'block';
+	}
+}
+
 setNumColorsTo1(){
+	const secondColor = document.querySelector('.two');
+	const thirdColor = document.querySelector('.three');
 	console.log("to 1 was called");
 	this.props.setColors(1);
 	console.log("Current state is:",this.props.numColors);
+	secondColor.style.display = 'none';
+	thirdColor.style.display = 'none';
 }
 
 setNumColorsTo2(){
+	const secondColor = document.querySelector('.two');
+	const thirdColor = document.querySelector('.three');
 	this.props.setColors(2);
 	console.log("Current state is:",this.props.numColors);
+	secondColor.style.display = 'block';
+	thirdColor.style.display = 'none';
 }
 
 setNumColorsTo3(){
+	const secondColor = document.querySelector('.two');
+	const thirdColor = document.querySelector('.three');
 	this.props.setColors(3);
 	console.log("Current state is:",this.props.numColors);
+	secondColor.style.display = 'block';
+	thirdColor.style.display = 'block';
 }
 
   render() {
