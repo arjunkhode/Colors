46930597617b1b45841ffd40dcac57fc87f69a69
arjunkhode <arjunkhode@gmail.com>
2017-05-13 14:23:01 -0700

    Set up a basic frame

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="app">
+      	Hi 
+      	<div className="colorFrame">
+      		<div className="colorComponent one">
+      		</div>
+      		<div className="colorComponent two">
+      		</div>
+      		<div className="colorComponent three">
+      		</div>
+      	</div>
+      	<div className="settings">
+      		<span> 1 color </span>
+      		<span> 2 colors </span>
+      		<span> 3 colors </span>
+
+      	</div>
+      </div>
     );
   }
 }
